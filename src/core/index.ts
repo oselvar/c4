bfaca11f967caa2466ee5ccd2cce26c91f7ba45b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2025-04-16 10:29:53 +0100

    Expose getUniqueCalls

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,7 @@
 export * from "./C4Model";
 export * from "./C4ModelBuilder";
 export * from "./decorators";
+export * from "./getUniqueCalls";
 export * from "./globals";
 export * from "./makeC4Model";
 export * from "./strings";
